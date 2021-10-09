6a7695a0da03d94b8b11f5978694f65df8daa501
sanghyuk han <jshans40@gmail.com>
2021-10-10 00:00:29 +0900

    feat : InputView, OutputView 추가
    InputView : 사용자의 입력값을 받는 View
    Outputview : console창에 띄어질 문구들

---
+++ src/main/java/racinggame/InputView.java
@@ -0,0 +1,13 @@
+package racinggame;
+
+import nextstep.utils.Console;
+
+public class InputView {
+    public String carNamesInput() {
+        return Console.readLine();
+    }
+
+    public int carTurnInput() {
+        return Integer.parseInt(Console.readLine());
+    }
+}
---
+++ src/main/java/racinggame/OutputView.java
@@ -0,0 +1,46 @@
+package racinggame;
+
+public class OutputView {
+    private final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String CAR_TURN_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final String GAME_ERROR_MESSAGE = "[ERROR] ";
+    private final String RACE_RESULT = "실행 결과";
+
+    public void printInputNames() {
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+    }
+
+    public void printInputTurn() {
+        System.out.println(CAR_TURN_INPUT_MESSAGE);
+    }
+
+    public void printError(String message) {
+        System.out.println(GAME_ERROR_MESSAGE + message);
+    }
+
+    public void printRaceResult(Cars cars) {
+        System.out.println(RACE_RESULT);
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getCarName() + " : " + printCarPosition(car.getPosition()));
+        }
+    }
+
+    private String printCarPosition(int position) {
+        StringBuilder carPositionBuilder = new StringBuilder();
+        for (int i=0; i<position; i++) {
+            carPositionBuilder.append("-");
+        }
+
+        return carPositionBuilder.toString();
+    }
+
+    public void printWinner(Cars cars) {
+        StringBuilder winnerStringBuilder = new StringBuilder();
+        for (Car car : cars.getWinnerCars()) {
+            winnerStringBuilder.append(car.getCarName()).append(",");
+        }
+
+        winnerStringBuilder.deleteCharAt(winnerStringBuilder.length() - 1);
+        System.out.printf("최종 우승자는 %s 입니다.", winnerStringBuilder.toString());
+    }
+}
