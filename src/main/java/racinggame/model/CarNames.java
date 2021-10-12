d4efd55e772d939783700e70ba9e7d7024d3f6a9
sanghyuk han <jshans40@gmail.com>
2021-10-12 18:38:49 +0900

    refactor(CarNames) : 생정자에서 컬렉션 객체 생성

--- src/main/java/racinggame/model/CarNames.java
+++ src/main/java/racinggame/model/CarNames.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class CarNames {
     private final int MINIMUM_CAR_NAMES = 2;
-    private List<CarName> carNames;
+    private final List<CarName> carNames;
 
     public CarNames(String carNamesString) {
+        carNames = new ArrayList<>();
+
         String[] carNameArray = carNamesString.split(",");
         if (carNameArray.length < MINIMUM_CAR_NAMES) {
             throw new IllegalArgumentException("자동차 이름은 2개 이상 필요합니다.");
@@ -17,8 +19,6 @@ public class CarNames {
     }
 
     public void createCarNames(String[] carNameArray) {
-        carNames = new ArrayList<>();
-
         for (String carName : carNameArray) {
             carNames.add(new CarName(carName));
         }
